c5e70d805338fb4ba080aaff07789d0b8c035075
Hyunji Seok <alleyji126@gmail.com>
2023-11-19 15:12:24 +0900

    feat(constants): initialize constants

---
+++ Baseball/src/constants/messages.js
@@ -0,0 +1,26 @@
+const INPUT = Object.freeze({
+  number: '숫자를 입력해주세요 : ',
+});
+
+const ERROR = Object.freeze({
+  not_a_valid_number: '유효하지 않은 숫자입니다. 다시 입력해 주세요.',
+});
+
+const OUTPUT = Object.freeze({
+  start: '숫자 야구 게임을 시작합니다.',
+  over: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  restart_or_exit: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+});
+
+const ZERO = 0;
+const ONE = 1;
+const TWO = 2;
+const THREE = 3;
+
+const BALL = '볼';
+const STRIKE = '스트라이크';
+const DEFAULT = '낫싱';
+
+const LINE_BREAK = '\n';
+
+export {INPUT, ERROR, OUTPUT, ZERO, ONE, TWO, THREE, BALL, STRIKE, DEFAULT, LINE_BREAK };
