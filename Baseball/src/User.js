7ff125c6e98c55398874308a646214e8af08aa0b
Hyunji Seok <alleyji126@gmail.com>
2023-11-19 23:03:27 +0900

    fix(User): update minor changes

--- Baseball/src/User.js
+++ Baseball/src/User.js
@@ -1,11 +1,8 @@
-import Validator from './validation/Validator.js';
-
 class User {
   #user;
 
   constructor(numbers) {
-    this.#user = Validator.validateNumbers(numbers);
-    Validator.validateDuplication(this.#user);
+    this.#user = numbers.split('').map(number => Number(number));
   }
 
   getUser() {
