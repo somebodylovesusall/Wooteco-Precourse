f009725e38157c34995abbb11e3e56c00bd3ceb1
Hyunji Seok <alleyji126@gmail.com>
2023-11-19 16:07:39 +0900

    feat(Computer): define class Computer

---
+++ Baseball/src/Computer.js
@@ -0,0 +1,20 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { THREE } from './constants/numbers.js';
+
+class Computer {
+  #computer = [];
+
+  pickRandomNumbers() {
+    while (this.#computer.length < THREE) {
+      const number = MissionUtils.Random.pickNumberInRange(ONE, NINE);
+
+      if (!this.#computer.includes(number)) {
+        this.#computer.push(number);
+      }
+    }
+
+    return this.#computer;
+  }
+}
+
+export default Computer;
