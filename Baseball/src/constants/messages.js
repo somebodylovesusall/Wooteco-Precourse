3eda4cf81dc08e2b3a7e1ea87341ea86f04020b0
Hyunji Seok <alleyji126@gmail.com>
2023-11-19 16:58:57 +0900

    feat(messages): update minor changes

--- Baseball/src/constants/messages.js
+++ Baseball/src/constants/messages.js
@@ -1,5 +1,6 @@
 const INPUT = Object.freeze({
   number: '숫자를 입력해주세요 : ',
+  restart_or_exit: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
 });
 
 const ERROR = Object.freeze({
@@ -8,8 +9,7 @@ const ERROR = Object.freeze({
 
 const OUTPUT = Object.freeze({
   start: '숫자 야구 게임을 시작합니다.',
-  over: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
-  restart_or_exit: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+  end: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
 });
 
 const BALL = '볼';
