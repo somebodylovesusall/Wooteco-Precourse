fb7c85887acacd5940c191fdd42b30316f13be9d
Hyunji Seok <alleyji126@gmail.com>
2023-11-26 13:56:55 +0900

    feat(numbers): add number constants

--- CarRacing/src/constants/numbers.js
+++ CarRacing/src/constants/numbers.js
@@ -1,4 +1,9 @@
 const START_INCLUSIVE = 0;
 const END_INCLUSIVE = 9;
+const ZERO = 0;
+const ONE = 1;
+const THREE = 3;
+const FOUR = 4;
+const NINE = 9;
 
-export { START_INCLUSIVE, END_INCLUSIVE };
+export { START_INCLUSIVE, END_INCLUSIVE, ZERO, ONE, THREE, FOUR, NINE };
