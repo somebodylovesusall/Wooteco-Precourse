fe19542dd8e5ef56e4ec9da726a19ec3b668ba09
Hyunji Seok <alleyji126@gmail.com>
2023-11-28 00:04:07 +0900

    test(Validator): update minor changes

--- Baseball/__tests__/ValidatorTest.js
+++ Baseball/__tests__/ValidatorTest.js
@@ -1,33 +1,33 @@
 import Validator from '../src/validation/Validator.js';
 
 describe('유효성 검증 테스트', () => {
-  test.each(['1234', '1.5', '1-5', '903'])('사용자가 세 자리의 자연수를 입력하지 않으면 예외가 발생한다.', numbers => {
+  test.each(['1234', '1.5', '1-5', '903'])('사용자가 세 자리 자연수를 입력하지 않으면 예외가 발생한다.', notThreeDigitNumber => {
     expect(() => {
-      Validator.validateNumbers(numbers);
+      Validator.validateThreeDigitNumber(notThreeDigitNumber);
     }).toThrow('[ERROR]');
   });
 
-  test('사용자가 중복된 세 자리의 자연수를 입력하면 예외가 발생한다.', () => {
+  test('사용자가 중복된 세 자리 자연수를 입력하면 예외가 발생한다.', () => {
     expect(() => {
-      Validator.validateNumbers('133');
+      Validator.validateThreeDigitNumber('133');
     }).toThrow('[ERROR]');
   });
 
-  test('사용자가 중복되지 않은 세 자리의 자연수를 입력하면 예외가 발생하지 않는다.', () => {
+  test('사용자가 중복되지 않은 세 자리 자연수를 입력하면 예외가 발생하지 않는다.', () => {
     expect(() => {
-      Validator.validateNumbers('123');
+      Validator.validateThreeDigitNumber('123');
     }).not.toThrow();
   });
 
   test('사용자가 1 또는 2를 입력하지 않으면 예외가 발생한다.', () => {
     expect(() => {
-      Validator.validateFlag('3');
+      Validator.validateRestartOrExit('3');
     }).toThrow('[ERROR]');
   });
 
-  test.each(['1', '2'])('사용자가 1 또는 2를 입력하면 예외가 발생하지 않는다.', flag => {
+  test.each(['1', '2'])('사용자가 1 또는 2를 입력하면 예외가 발생하지 않는다.', restartOrExit => {
     expect(() => {
-      Validator.validateFlag(flag);
+      Validator.validateRestartOrExit(restartOrExit);
     }).not.toThrow();
   });
 });
