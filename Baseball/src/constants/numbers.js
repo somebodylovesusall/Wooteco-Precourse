5c30960e7236cafe26a16f87192dcfd561bfa3fa
Hyunji Seok <alleyji126@gmail.com>
2023-11-27 23:57:47 +0900

    fix(constants): rename and remove constants

--- Baseball/src/constants/numbers.js
+++ Baseball/src/constants/numbers.js
@@ -1,7 +1,10 @@
 const ZERO = 0;
 const ONE = 1;
-const TWO = 2;
 const THREE = 3;
-const NINE = 9;
+const START_INCLUSIVE = 1;
+const END_INCLUSIVE = 9;
+const RESTART = 1;
+const EXIT = 2;
+const DIGIT = 3;
 
-export { ZERO, ONE, TWO, THREE, NINE };
+export { ZERO, ONE, THREE, START_INCLUSIVE, END_INCLUSIVE, RESTART, EXIT, DIGIT };
