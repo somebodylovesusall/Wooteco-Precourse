7c7d10acaa14ce5d0eadbb747f9d914f9bb5397e
Hyunji Seok <alleyji126@gmail.com>
2023-11-29 23:27:19 +0900

    fix(Output): update minor changes

--- CarRacing/src/views/Output.js
+++ CarRacing/src/views/Output.js
@@ -1,15 +1,14 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import { ONE } from '../constants/numbers.js';
 import { COMMA, COLON, HYPHEN, LINE_BREAK, OUTPUT } from '../constants/messages.js';
 
 const Output = {
-  printGameResult(count, cars) {
-    if (count === ONE) {
-      MissionUtils.Console.print(`${LINE_BREAK}${OUTPUT.game_result}`);
-    }
+  printGameResult() {
+    MissionUtils.Console.print(`${LINE_BREAK}${OUTPUT.game_result}`);
+  },
 
-    Object.entries(cars).forEach(([key, value]) => {
-      MissionUtils.Console.print(`${key} ${COLON} ${HYPHEN.repeat(value)}`);
+  printRaceResult(cars) {
+    Object.entries(cars).forEach(([car, result]) => {
+      MissionUtils.Console.print(`${car} ${COLON} ${HYPHEN.repeat(result)}`);
     });
 
     MissionUtils.Console.print(LINE_BREAK);
