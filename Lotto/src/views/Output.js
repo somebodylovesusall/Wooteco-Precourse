7b54016dfdd14ac000db2398797b2f4e3a8f8346
Hyunji Seok <alleyji126@gmail.com>
2023-12-03 15:57:33 +0900

    feat(Output): implement printStatistics, printGrossProfit

--- Lotto/src/views/Output.js
+++ Lotto/src/views/Output.js
@@ -1,19 +1,25 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import { COMMA, COUNT, LOTTOS, NEWLINE } from '../constants/messages.js';
+import { COMMA, NEWLINE, OUTPUT } from '../constants/messages.js';
+import { ONE } from '../constants/numbers.js';
 
 const Output =  {
   printLottos(lottos) {
     const totalLottos = lottos.map(lotto => `[${lotto.join(`${COMMA} `)}]`).join(NEWLINE);
-    MissionUtils.Console.print(`${NEWLINE}${lottos.length}${LOTTOS}${NEWLINE}${totalLottos}`);
+    MissionUtils.Console.print(`${NEWLINE}${lottos.length}${OUTPUT.lottos}${NEWLINE}${totalLottos}`);
     MissionUtils.Console.print(`${NEWLINE}`);
   },
 
-  printStatistics() {
-
+  printStatistics(matches) {
+    MissionUtils.Console.print(OUTPUT.statistics);
+    MissionUtils.Console.print(`${OUTPUT.three_match}${matches.three}${OUTPUT.count}`);
+    MissionUtils.Console.print(`${OUTPUT.four_match}${matches.four}${OUTPUT.count}`);
+    MissionUtils.Console.print(`${OUTPUT.five_match}${matches.five}${OUTPUT.count}`);
+    MissionUtils.Console.print(`${OUTPUT.five_plus_bonus_match}${matches.fivePlusBonus}${OUTPUT.count}`);
+    MissionUtils.Console.print(`${OUTPUT.six_match}${matches.six}${OUTPUT.count}`);
   },
 
-  printGrossProfit() {
-
+  printGrossProfit(grossProfit) {
+    MissionUtils.Console.print(`${OUTPUT.gross_profit}${grossProfit.toFixed(ONE).replace(/\B(?=(\d{3})+(?!\d))/g, COMMA)}${OUTPUT.percent}`);
   },
 }
 
