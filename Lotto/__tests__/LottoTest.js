7b865fe9eae451f8d156f011d121c3b3b8b4477e
Hyunji Seok <alleyji126@gmail.com>
2023-12-03 16:38:24 +0900

    test(Lotto): implement test case for makeLottos

--- Lotto/__tests__/LottoTest.js
+++ Lotto/__tests__/LottoTest.js
@@ -1,18 +1,14 @@
 import Lotto from "../src/Lotto.js";
 
 describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+  let lotto;
+  
+  beforeEach(() => {
+    lotto = new Lotto(8000);
   });
 
-  // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
-    expect(() => {
-      new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+  test("로또 수량만큼 로또를 발행한다.", () => {
+    const lottos = lotto.makeLottos();
+    expect(lottos.length).toEqual(8);
   });
-
-  // 아래에 추가 테스트 작성 가능
 });
