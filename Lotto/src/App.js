c0ac90cd9826842005bd4276eb3ff1c521b95c93
Hyunji Seok <alleyji126@gmail.com>
2023-12-03 15:59:14 +0900

    feat(App): set function play

--- Lotto/src/App.js
+++ Lotto/src/App.js
@@ -1,5 +1,15 @@
+import Machine from './Machine.js';
+
 class App {
-  async play() {}
+  async play() {
+    const machine = new Machine();
+    await machine.playLotto();
+    const winning = await machine.makeWinning();
+    await machine.makeBonus(winning);
+    const matches = machine.calculateMatches();
+    const grossProfit = machine.calculateGrossProfit(matches);
+    machine.showStatistics(matches, grossProfit);
+  }
 }
 
 export default App;
