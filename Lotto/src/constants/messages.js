0b5bb192b749f1d362d5efe06cb1058717a22933
Hyunji Seok <alleyji126@gmail.com>
2023-12-03 21:26:54 +0900

    refactor(messages): add Object.freeze

--- Lotto/src/constants/messages.js
+++ Lotto/src/constants/messages.js
@@ -13,7 +13,7 @@ const ERROR = Object.freeze({
   not_a_valid_bonus: '[ERROR] 유효하지 않은 보너스 번호입니다. 다시 입력해 주세요.',
 });
 
-const OUTPUT = {
+const OUTPUT = Object.freeze({
   three_match: '3개 일치 (5,000원) - ',
   four_match: '4개 일치 (50,000원) - ',
   five_match: '5개 일치 (1,500,000원) - ',
@@ -24,6 +24,6 @@ const OUTPUT = {
   statistics: `${NEWLINE}당첨 통계${NEWLINE}---`,
   gross_profit: '총 수익률은 ',
   percent: '%입니다.',
-};
+});
 
 export { INPUT, ERROR, OUTPUT, NEWLINE, COMMA };
