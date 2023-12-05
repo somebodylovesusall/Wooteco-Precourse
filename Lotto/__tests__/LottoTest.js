f211ec7367f94a041dea21a4e09a9899317afc10
Hyunji Seok <alleyji126@gmail.com>
2023-12-05 13:03:58 +0900

    style(LottoTest): remove double quotation marks

--- Lotto/__tests__/LottoTest.js
+++ Lotto/__tests__/LottoTest.js
@@ -1,13 +1,13 @@
-import Lotto from "../src/Lotto.js";
+import Lotto from '../src/Lotto.js';
 
-describe("로또 클래스 테스트", () => {
+describe('로또 클래스 테스트', () => {
   let lotto;
   
   beforeEach(() => {
     lotto = new Lotto(8000);
   });
 
-  test("로또 수량만큼 로또를 발행한다.", () => {
+  test('로또 수량만큼 로또를 발행한다.', () => {
     const lottos = lotto.makeLottos();
     expect(lottos.length).toEqual(8);
   });
