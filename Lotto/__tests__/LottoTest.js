96a0c07b67c4bd5ebb6a74fead4ee5927d114b49
Hyunji Seok <alleyji126@gmail.com>
2023-12-06 14:41:00 +0900

    test(Lotto): edit test case

--- Lotto/__tests__/LottoTest.js
+++ Lotto/__tests__/LottoTest.js
@@ -4,11 +4,11 @@ describe('로또 클래스 테스트', () => {
   let lotto;
   
   beforeEach(() => {
-    lotto = new Lotto(8000);
+    lotto = new Lotto();
   });
 
   test('로또 수량만큼 로또를 발행한다.', () => {
-    const lottos = lotto.makeLottos();
+    const lottos = lotto.makeLottos(8000);
     expect(lottos.length).toEqual(8);
   });
 });
